73fb893d3a9f4991deca5e19e472cff1c2d16244
tomas <tchenko.ok@gmail.com>
2026-07-31 16:43:37 +0800

    fix: make CMS-generated docs self-contained

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
-import { CmsHtml } from '@site/src/components/CmsHtml';
 import { ImageView } from '@site/src/components/ImageView';
 
 export default {
     ...MDXComponents,
-
-    CmsHtml,
+    
     ImageView,
 };
